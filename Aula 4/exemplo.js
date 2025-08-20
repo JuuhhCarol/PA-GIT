82edfa35033d320b77ec48aa0736d765bf1bde90
juuhhcarol <juliecaroe17@gmail.com>
2025-08-20 11:50:47 -0300

    aula 4

---
+++ Aula 4/exemplo.js
@@ -0,0 +1,10 @@
+const trocarCor =() =>{
+    let quadrado = document.getElementById('quadrado');
+    let  cor = document.getElementById('color').value;
+    quadrado.style.backgroundColor=cor;
+}
+
+const receberCor =() =>{
+let inputColor = document.getElementById('el2');
+console.log(receberCor)
+}
